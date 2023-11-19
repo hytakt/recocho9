b40dfa4f993a5b040629ef6ce7c120766dfa8c8d
hamada1119 <76606177+hamada1119@users.noreply.github.com>
2023-11-19 14:11:08 +0900

    feat:色付け

--- recocho9/color_change.js
+++ recocho9/color_change.js
@@ -1,26 +1,15 @@
 const audioElement = document.getElementById('myAudio')
-
-let currentIndex = 0
 audioElement.addEventListener('play', function () {
   console.log('再生が開始されました！')
-  /*
-  一文字ずつ変化させる実装
-  clearInterval(intervalId
-  intervalId = setInterval(function () {
-    if (currentIndex < textContent.length) {
-      // Get the current character
-      const currentChar = textContent[currentIndex]
+  //line1=22s
+  const line1Element = document.getElementById('line1')
+  const textContent1 = line1Element.textContent
+  const spanElement = document.createElement('span')
+  spanElement.textContent = textContent1
 
-      // Change the background color of the current character
-      el.innerHTML =
-        textContent.substring(0, currentIndex) +
-        `<span style="background-color: red;">${currentChar}</span>` +
-        textContent.substring(currentIndex + 1)
-
-      currentIndex++
-    } else {
-      // Clear the interl when all characters have been processed
-      clearInterval(intervalId)
-    }
-  }, 1000) // 1000 milliseconds = 1 second*/
+  setTimeout(function () {
+    spanElement.style.backgroundColor = 'red'
+  }, 3000) // 3000 milliseconds = 3 seconds
+  line1Element.innerHTML = ''
+  line1Element.appendChild(spanElement)
 })
